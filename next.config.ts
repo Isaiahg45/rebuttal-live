5a113fa22d9f52b783b795c37a297e37c62bdc8f
zaysales442-sketch <zaysales442@gmail.com>
2026-06-09 15:35:26 -0400

    remove CSP header causing inline style conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; default-src *; connect-src * wss://*.agora.io wss://*.sd-rtn.com; media-src *; worker-src blob:;",          },
-        ],
-      },
-    ]
-  },
-}; 
+};
 
 export default nextConfig;
